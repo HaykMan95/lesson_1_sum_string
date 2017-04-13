56c6584e235c010c3c98d254241beb39521a5cb4
hripman <rips.manukyan@gmail.com>
2017-04-13 14:44:01 +0400

    sum string function

---
+++ first.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rips on 12.04.2017.
+ */
+console.log("this is new branch js file");
+
+function sumString(x,y) {
+    if (+x && +y) {
+        return ((+x) + (+y)).toString();
+    }
+}
+console.log(sumString("4", "4"));
+
